a2dd0ab73ef3950dcf94ad5be09e40a2a7c1ca9c
shlegel <sveta.shlegel@mail.ru>
2023-10-02 21:50:23 +0300

    homework-3

--- courses/permissions.py
+++ courses/permissions.py
@@ -1,32 +1,6 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
-class IsOwnerOrStaff(BasePermission):
-
-    def has_permission(self, request, view):
-        if request.user.is_staff:
-            return True
-
-        return request.user == view.get_object().owner
-
-
-class IsAuthCreate(BasePermission):
-
-    def has_permission(self, request, view):
-        if request.method == "POST" and request.user.is_authenticated:
-            return True
-        elif request.method == "POST" and not request.user.is_authenticated:
-            return False
-        elif request.method in SAFE_METHODS:
-            return True
-
-    def has_object_permission(self, request, view, obj):
-        if request.user.is_staff:
-            return True
-
-        return request.user == view.get_object().owner
-
-
 class IsOwnerOrModerator(BasePermission):
 
     def has_permission(self, request, view):
